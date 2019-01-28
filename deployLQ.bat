e4369b4f9c0e6fee96e0de93de77142e114179a7
Sze Howe Koh <szehowe.koh@gmail.com>
2019-01-29 07:47:47 +0800

    Install package directly into the LabVIEW installation folder
    - This makes the examples visible in the LabVIEW Example Finder
    - LVProj-based build specs don't support this, so we call nipkg.exe
      directly instead
    - Also, use the deployment script to copy LQ.dll instead of relying on
      qmake's install feature, to keep the automation tasks in one place

--- deployLQ.bat
+++ deployLQ.bat
@@ -1,18 +1,27 @@
+REM Ensure that your compiler is found in the PATH (e.g. by calling this script via
+REM the Developer Command Prompt for VS 2017)
+
 @ECHO off
 
 REM ==================================================================================
 REM (Edit these parameters before running this script, if necessary)
 REM ----------------------------------------------------------------------------------
-SET QT_ROOT=C:\Qt\Online\5.12.0\msvc2017
+SET QT_ROOT=C:\Qt\5.12.0\msvc2017
 SET QWT_ROOT=C:\Qwt-6.1.3
 SET MAKE=nmake
-SET LV_EXE_PATH="C:\Program Files (x86)\National Instruments\LabVIEW 2014\LabVIEW.exe"
+SET NIPKG_EXE="C:\Program Files\National Instruments\NI Package Manager\nipkg.exe"
+SET LV_EXE="C:\Program Files (x86)\National Instruments\LabVIEW 2014\LabVIEW.exe"
 SET LV_PORT=3363
 REM ==================================================================================
 
 REM Set absolute paths according to this project's folder structure
 SET LQ_DEV_ROOT=%cd%
-SET TARGET_DIR=%LQ_DEV_ROOT%\builds\LQ
+SET BUILD_DIR=%LQ_DEV_ROOT%\builds
+SET OUTPUT_DIR=%BUILD_DIR%\LQ
+SET CPP_DIR=%BUILD_DIR%\Cpp
+SET PKG_DIR=%OUTPUT_DIR%\Package
+SET PKG_DATA_DIR=%PKG_DIR%\data\ni-paths-LV2014DIR
+SET VI_LIB_DIR=%PKG_DATA_DIR%\vi.lib\addons\LQ
 
 
 REM Reset ERRORLEVEL to 0 before starting
@@ -20,55 +29,57 @@ ver > nul
 
 
 REM Clean out the distribution folder
-IF EXIST %TARGET_DIR% ( rmdir %TARGET_DIR% /S /Q )
+IF EXIST %OUTPUT_DIR% ( rmdir %OUTPUT_DIR% /S /Q )
 IF NOT %ERRORLEVEL%==0 GOTO End
 
 
 REM Prepare shadow build folder for shared library
-IF NOT EXIST builds\Cpp mkdir builds\Cpp
-cd builds\Cpp\
+IF NOT EXIST %CPP_DIR% mkdir %CPP_DIR%
+cd %CPP_DIR%
 IF NOT %ERRORLEVEL%==0 GOTO End
 
 
-REM Build and copy LQ shared library
+REM Build LQ shared library
 %QT_ROOT%\bin\qmake.exe %LQ_DEV_ROOT%\src\Cpp\
 %MAKE%
-%MAKE% install
 IF NOT %ERRORLEVEL%==0 ( cd %LQ_DEV_ROOT% && EXIT /B -1 )
 cd %LQ_DEV_ROOT%
 
 
-REM Copy other files
-robocopy %LQ_DEV_ROOT%\3rdparty\     %TARGET_DIR%\3rdparty\ /E
-robocopy %LQ_DEV_ROOT%\examples\     %TARGET_DIR%\examples\ /E
-robocopy %LQ_DEV_ROOT%\src\LabVIEW\  %TARGET_DIR%\src\LabVIEW\ /E
-COPY     %LQ_DEV_ROOT%\LICENSE.MPLv2 %TARGET_DIR%\
-COPY     %LQ_DEV_ROOT%\README.md     %TARGET_DIR%\
-COPY     %QWT_ROOT%\lib\qwt.dll      %TARGET_DIR%\bin\
-%QT_ROOT%\bin\windeployqt.exe --no-translations --no-system-d3d-compiler --no-compiler-runtime --no-angle --no-opengl-sw %TARGET_DIR%\bin\LQ.dll
-%QT_ROOT%\bin\windeployqt.exe --no-translations --no-system-d3d-compiler --no-compiler-runtime --no-angle --no-opengl-sw %TARGET_DIR%\bin\qwt.dll
+REM Copy files
+robocopy %LQ_DEV_ROOT%\examples\     %PKG_DATA_DIR%\examples\LQ\ /E
+robocopy %LQ_DEV_ROOT%\3rdparty\     %VI_LIB_DIR%\3rdparty\ /E
+robocopy %LQ_DEV_ROOT%\src\LabVIEW\  %VI_LIB_DIR%\ /E
+COPY     %LQ_DEV_ROOT%\LICENSE.MPLv2 %VI_LIB_DIR%\
+COPY     %LQ_DEV_ROOT%\README.md     %VI_LIB_DIR%\
+mkdir                                %VI_LIB_DIR%\bin\
+COPY     %CPP_DIR%\release\LQ.dll    %VI_LIB_DIR%\bin\
+COPY     %QWT_ROOT%\lib\qwt.dll      %VI_LIB_DIR%\bin\
+%QT_ROOT%\bin\windeployqt.exe --no-translations --no-system-d3d-compiler --no-compiler-runtime --no-angle --no-opengl-sw %VI_LIB_DIR%\bin\LQ.dll
+%QT_ROOT%\bin\windeployqt.exe --no-translations --no-system-d3d-compiler --no-compiler-runtime --no-angle --no-opengl-sw %VI_LIB_DIR%\bin\qwt.dll
 IF NOT %ERRORLEVEL%==0 GOTO End
 
 
-REM Discard unnecessary files (local temporary files, example README files, object files)
-DEL /S %TARGET_DIR%\*.aliases
-DEL /S %TARGET_DIR%\*.lvlps
-DEL /S %TARGET_DIR%\bin\LQ.lib
-DEL /S %TARGET_DIR%\examples\*README.md
+REM Discard unnecessary files (local temporary files, example README files)
+DEL /S %PKG_DIR%\*.aliases
+DEL /S %PKG_DIR%\*.lvlps
+DEL /S %PKG_DATA_DIR%\examples\*README.md
 
 
 REM Un-separate compiled code from source to greatly boost performance for library users
-LabVIEWCLI.exe -LabVIEWPath %LV_EXE_PATH% -PortNumber %LV_PORT% -OperationName RunVI -VIPath "%LQ_DEV_ROOT%\utils\CLI_Disable Source Separation.vi" %TARGET_DIR%\src\LabVIEW\
+LabVIEWCLI.exe -LabVIEWPath %LV_EXE% -PortNumber %LV_PORT% -OperationName RunVI -VIPath "%LQ_DEV_ROOT%\utils\CLI_Disable Source Separation.vi" %VI_LIB_DIR%\
 IF NOT %ERRORLEVEL%==0 GOTO End
 
 
-REM Execute the LabVIEW build specs
-LabVIEWCLI.exe -LabVIEWPath %LV_EXE_PATH% -PortNumber %LV_PORT% -OperationName ExecuteBuildSpec -ProjectPath %LQ_DEV_ROOT%\utils\LQ-Packager.lvproj -BuildSpecName "Source Distribution"
-IF NOT %ERRORLEVEL%==0 GOTO End
-
-LabVIEWCLI.exe -LabVIEWPath %LV_EXE_PATH% -PortNumber %LV_PORT% -OperationName ExecuteBuildSpec -ProjectPath %LQ_DEV_ROOT%\utils\LQ-Packager.lvproj -BuildSpecName "Package"
+REM Write the package metadata (modelled after the Debian/Opkg format)
+LabVIEWCLI.exe -LabVIEWPath %LV_EXE% -PortNumber %LV_PORT% -OperationName RunVI -VIPath "%LQ_DEV_ROOT%\utils\CLI_Write Nipkg Metadata.vi" %PKG_DIR%\ 2014 0.2.0 x86
 IF NOT %ERRORLEVEL%==0 GOTO End
 
 
-ECHO Done. See builds\LQ-Packager\ for the output files.
+REM Create the final package
+%NIPKG_EXE% pack %PKG_DIR%\ %OUTPUT_DIR%
+IF NOT %ERRORLEVEL%==0 GOTO End
+
+
+ECHO Done. The output files are in %OUTPUT_DIR%.
 :End
