e31f8cae260cfe2076bc2e97a661875208e20e06
Sze Howe Koh <szehowe.koh@gmail.com>
2019-02-01 22:26:26 +0800

    Add ability to generate packages for multiple LabVIEW versions
    Individual packages for LV 2014 - LV 2018 are now mass-producible.
    
    (I'm starting to regret using a Batch script instead of PowerShell or
    Python. Requiring devs to change the Execution Policy or install Python
    might be nicer than the archaic Batch syntax!)

--- deployLQ.bat
+++ deployLQ.bat
@@ -12,6 +12,8 @@ SET MAKE=nmake
 SET NIPKG_EXE="C:\Program Files\National Instruments\NI Package Manager\nipkg.exe"
 SET LV_EXE="C:\Program Files (x86)\National Instruments\LabVIEW 2014\LabVIEW.exe"
 SET LV_PORT=3363
+SET LV_YEAR_START=2014
+SET LV_YEAR_END=2018
 REM ==================================================================================
 
 REM Set absolute paths according to this project's folder structure
@@ -20,7 +22,7 @@ SET BUILD_DIR=%LQ_DEV_ROOT%\builds
 SET OUTPUT_DIR=%BUILD_DIR%\LQ
 SET CPP_DIR=%BUILD_DIR%\Cpp
 SET PKG_DIR=%OUTPUT_DIR%\Package
-SET PKG_DATA_DIR=%PKG_DIR%\data\ni-paths-LV2014DIR
+SET PKG_DATA_DIR=%PKG_DIR%\data\ni-paths-LV%LV_YEAR_START%DIR
 SET VI_LIB_DIR=%PKG_DATA_DIR%\vi.lib\addons\LQ
 
 
@@ -30,13 +32,13 @@ ver > nul
 
 REM Clean out the distribution folder
 IF EXIST %OUTPUT_DIR% ( rmdir %OUTPUT_DIR% /S /Q )
-IF NOT %ERRORLEVEL%==0 GOTO End
+IF NOT %ERRORLEVEL%==0 GOTO :eof
 
 
 REM Prepare shadow build folder for shared library
 IF NOT EXIST %CPP_DIR% mkdir %CPP_DIR%
 cd %CPP_DIR%
-IF NOT %ERRORLEVEL%==0 GOTO End
+IF NOT %ERRORLEVEL%==0 GOTO :eof
 
 
 REM Build LQ shared library
@@ -57,7 +59,7 @@ COPY     %CPP_DIR%\release\LQ.dll    %VI_LIB_DIR%\bin\
 COPY     %QWT_ROOT%\lib\qwt.dll      %VI_LIB_DIR%\bin\
 %QT_ROOT%\bin\windeployqt.exe --no-translations --no-system-d3d-compiler --no-compiler-runtime --no-angle --no-opengl-sw %VI_LIB_DIR%\bin\LQ.dll
 %QT_ROOT%\bin\windeployqt.exe --no-translations --no-system-d3d-compiler --no-compiler-runtime --no-angle --no-opengl-sw %VI_LIB_DIR%\bin\qwt.dll
-IF NOT %ERRORLEVEL%==0 GOTO End
+IF NOT %ERRORLEVEL%==0 GOTO :eof
 
 
 REM Discard unnecessary files (local temporary files, example README files)
@@ -68,18 +70,39 @@ DEL /S %PKG_DATA_DIR%\examples\*README.md
 
 REM Un-separate compiled code from source to greatly boost performance for library users
 LabVIEWCLI.exe -LabVIEWPath %LV_EXE% -PortNumber %LV_PORT% -OperationName RunVI -VIPath "%LQ_DEV_ROOT%\utils\CLI_Disable Source Separation.vi" %VI_LIB_DIR%\
-IF NOT %ERRORLEVEL%==0 GOTO End
+IF NOT %ERRORLEVEL%==0 GOTO :eof
 
+
+REM Build a package for each LabVIEW version
+FOR /L %%Y IN (%LV_YEAR_START%,1,%LV_YEAR_END%) DO (
+	CALL :packOneVersion %%Y
+	IF NOT %ERRORLEVEL%==0 GOTO :eof
+)
+
+
+ECHO Done. The output files are in %OUTPUT_DIR%.
+GOTO :eof
+
+
+REM ==================================================================================
+REM Subroutine for making one package
+REM ==================================================================================
+:packOneVersion
 
 REM Write the package metadata (modelled after the Debian/Opkg format)
-LabVIEWCLI.exe -LabVIEWPath %LV_EXE% -PortNumber %LV_PORT% -OperationName RunVI -VIPath "%LQ_DEV_ROOT%\utils\CLI_Write Nipkg Metadata.vi" %PKG_DIR%\ 2014 0.2.0 x86
-IF NOT %ERRORLEVEL%==0 GOTO End
+LabVIEWCLI.exe -LabVIEWPath %LV_EXE% -PortNumber %LV_PORT% -OperationName RunVI -VIPath "%LQ_DEV_ROOT%\utils\CLI_Write Nipkg Metadata.vi" %PKG_DIR%\ %1 0.2.0 x86
+IF NOT %ERRORLEVEL%==0 GOTO :eof
 
 
 REM Create the final package
 %NIPKG_EXE% pack %PKG_DIR%\ %OUTPUT_DIR%
-IF NOT %ERRORLEVEL%==0 GOTO End
+IF NOT %ERRORLEVEL%==0 GOTO :eof
 
 
-ECHO Done. The output files are in %OUTPUT_DIR%.
-:End
+REM Prepare for next year
+SET OLD=%PKG_DIR%\data\ni-paths-LV%1DIR
+SET /A NEXT_YEAR=%1+1
+SET NEW=%PKG_DIR%\data\ni-paths-LV%NEXT_YEAR%DIR
+MOVE %OLD% %NEW%
+
+EXIT /B
